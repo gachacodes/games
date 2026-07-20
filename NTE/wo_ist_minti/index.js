27efcf5ed60e7d2019d022ab739546b28a0693b2
gachacodes <cubethefirst@gmail.com>
2026-07-20 19:53:02 +0200

    Add files via upload

---
+++ NTE/wo_ist_minti/index.js
@@ -0,0 +1,423 @@
+const minti_text = "Wo ist Minti? Nr. ";
+        const bilderDaten = [
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_1.png",
+                frage: minti_text+"1"+" - Leicht",
+                x: 74, 
+                y: 44, 
+                breite: 2, 
+                hoehe: 6,
+                bagel_id: 12212639
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_2.png",
+                frage: minti_text+"2"+" - Leicht",
+                x: 66, 
+                y: 34, 
+                breite: 2, 
+                hoehe: 6,
+                bagel_id: 12212802
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_3.png",
+                frage: minti_text+"3"+" - Mittel",
+                x: 14, 
+                y: 61, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12213042
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_4.png",
+                frage: minti_text+"4"+" - Leicht",
+                x: 86, 
+                y: 34, 
+                breite: 2, 
+                hoehe: 6,
+                bagel_id: 12213180
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_5.png",
+                frage: minti_text+"5"+" - Leicht",
+                x: 88, 
+                y: 35, 
+                breite: 2, 
+                hoehe: 6,
+                bagel_id: 12213319
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_6.png",
+                frage: minti_text+"6"+" - Mittel",
+                x: 66, 
+                y: 39, 
+                breite: 2, 
+                hoehe: 6,
+                bagel_id: 12359315
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_7.png",
+                frage: minti_text+"7"+" - Leicht",
+                x: 5, 
+                y: 86, 
+                breite: 3, 
+                hoehe: 9,
+                bagel_id: 12440944
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_8.png",
+                frage: minti_text+"8"+" - Mittel",
+                x: 58, 
+                y: 36, 
+                breite: 2, 
+                hoehe: 6,
+                bagel_id: 12441040
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_9.png",
+                frage: minti_text+"9"+" - Schwer",
+                x: 71, 
+                y: 67, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12441157
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_10.png",
+                frage: minti_text+"10"+" - Schwer",
+                x: 57, 
+                y: 49, 
+                breite: 2, 
+                hoehe: 6,
+                bagel_id: 12441252
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_11.png",
+                frage: minti_text+"11"+" - Leicht",
+                x: 36, 
+                y: 35, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12500231
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_12.png",
+                frage: minti_text+"12"+" - Schwer",
+                x: 50, 
+                y: 37, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12500399
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_13.png",
+                frage: minti_text+"13"+" - Mittel",
+                x: 76, 
+                y: 28, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12500564
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_14.png",
+                frage: minti_text+"14"+" - Schwer",
+                x: 69, 
+                y: 18, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12500863
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_15.png",
+                frage: minti_text+"15"+" - Schwer",
+                x: 79, 
+                y: 28, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12501114
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_16.png",
+                frage: minti_text+"16"+" - Schwer",
+                x: 61, 
+                y: 55, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12566274
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_17.png",
+                frage: minti_text+"17"+" - Schwer",
+                x: 12, 
+                y: 22, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12566566
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_18.png",
+                frage: minti_text+"18"+" - Schwer",
+                x: 73, 
+                y: 50, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12566689
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_19.png",
+                frage: minti_text+"19"+" - Schwer",
+                x: 21, 
+                y: 65, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12566858
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_20.png",
+                frage: minti_text+"20"+" - Schwer",
+                x: 41, 
+                y: 35, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12566940
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_21.png",
+                frage: minti_text+"21"+" - Mittel",
+                x: 32, 
+                y: 43, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12688512
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_22.png",
+                frage: minti_text+"22"+" - Mittel",
+                x: 79, 
+                y: 41, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12688586
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_23.png",
+                frage: minti_text+"23"+" - Mittel",
+                x: 40, 
+                y: 34, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12688647
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_24.png",
+                frage: minti_text+"24"+" - Schwer",
+                x: 72, 
+                y: 40, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12688716
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_25.png",
+                frage: minti_text+"25"+" - Schwer",
+                x: 97.5, 
+                y: 25, 
+                breite: 3, 
+                hoehe: 10,
+                bagel_id: 12688812
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_26.png",
+                frage: minti_text+"26"+" - Mittel",
+                x: 42, 
+                y: 33, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12748027
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_27.png",
+                frage: minti_text+"27"+" - Mittel",
+                x: 25, 
+                y: 42, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12748213
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_28.png",
+                frage: minti_text+"28"+" - Mittel",
+                x: 74, 
+                y: 37, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12748519
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_29.png",
+                frage: minti_text+"29"+" - Mittel",
+                x: 5.5, 
+                y: 51, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12748877
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_30.png",
+                frage: minti_text+"30"+" - Schwer",
+                x: 72, 
+                y: 32, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12881363
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_31.png",
+                frage: minti_text+"31"+" - Mittel",
+                x: 23, 
+                y: 52, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12881481
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_32.png",
+                frage: minti_text+"32"+" - Schwer",
+                x: 97.5, 
+                y: 35, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12881564
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_33.png",
+                frage: minti_text+"33"+" - Mittel",
+                x: 2, 
+                y: 37.5, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12881731
+            },
+            {
+                src: "../../images/games/NTE/wo_ist_minti/wo_ist_minti_34.png",
+                frage: minti_text+"34"+" - Leicht",
+                x: 0, 
+                y: 53, 
+                breite: 3, 
+                hoehe: 6,
+                bagel_id: 12881847
+            },
+        ];
+
+        
+        const gridAnsicht = document.getElementById("ansicht-grid");
+        const spielAnsicht = document.getElementById("ansicht-spiel");
+        const bagel_text = document.getElementById("bagel");
+        
+        const imgElement = document.getElementById("suchbild");
+        const aufgabeElement = document.getElementById("aufgabe");
+        const loesungElement = document.getElementById("loesung");
+        
+        const btnLoesen = document.getElementById("btn-loesen");
+        const btnZurueck = document.getElementById("btn-zurueck");
+
+        let loesungSichtbar = false;
+
+        bagel_text.addEventListener("click", () => {
+    
+    const idZumKopieren = bagel_text.dataset.id;
+    
+    
+    navigator.clipboard.writeText(idZumKopieren).then(() => {
+        
+        const originalText = bagel_text.innerText;
+        
+        
+        bagel_text.innerText = "Kopiert";
+        bagel_text.style.color = "#28a745"; 
+        
+        
+        setTimeout(() => {
+            bagel_text.innerText = originalText;
+            bagel_text.style.color = ""; 
+        }, 1500);
+    }).catch(err => {
+        console.error("Fehler beim Kopieren: ", err);
+        alert("Kopieren fehlgeschlagen!");
+    });
+});
+
+        
+        function generiereGrid() {
+            gridAnsicht.innerHTML = ""; 
+            
+            bilderDaten.forEach((daten, index) => {
+                
+                const item = document.createElement("div");
+                item.className = "grid-item";
+                
+                
+                item.onclick = () => starteSpiel(index);
+                
+                
+                item.innerHTML = `
+                    <img src="${daten.src}" alt="Vorschau">
+                    <div class="grid-text">${daten.frage}</div>
+                `;
+                
+                gridAnsicht.appendChild(item);
+            });
+        }
+
+        
+        function starteSpiel(index) {
+            const daten = bilderDaten[index];
+            
+            bagel_text.style.display = "block";
+            gridAnsicht.style.display = "none";
+            spielAnsicht.style.display = "block";
+            
+            bagel.innerText = "Bagel-ID: "+daten.bagel_id;
+            bagel_text.dataset.id = daten.bagel_id;
+            imgElement.src = daten.src;
+            aufgabeElement.innerText = daten.frage;
+
+            loesungElement.style.left = daten.x + "%";
+            loesungElement.style.top = daten.y + "%";
+            loesungElement.style.width = daten.breite + "%";
+            loesungElement.style.height = daten.hoehe + "%";
+
+            setzeLoesungZustand(false);
+            
+        }
+
+        
+        btnZurueck.addEventListener("click", () => {
+            bagel_text.style.display = "none";
+            spielAnsicht.style.display = "none";
+            gridAnsicht.style.display = "grid"; 
+            
+            
+            imgElement.src = "";
+            setzeLoesungZustand(false);
+        });
+
+        function setzeLoesungZustand(sichtbar) {
+            loesungSichtbar = sichtbar;
+            
+            if (loesungSichtbar) {
+                loesungElement.style.display = "block";
+                btnLoesen.innerText = "Lösung ausblenden";
+                btnLoesen.classList.add("active");
+            }else{ 
+                loesungElement.style.display = "none";
+                btnLoesen.innerText = "Wo ist Minti?";
+                btnLoesen.classList.remove("active");
+            }
+        }
+
+        btnLoesen.addEventListener("click", () => {
+            setzeLoesungZustand(!loesungSichtbar);
+            
+        });
+
+        generiereGrid();
